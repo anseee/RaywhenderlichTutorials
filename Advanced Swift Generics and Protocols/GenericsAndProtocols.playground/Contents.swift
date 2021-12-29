05008e751dc360bf0ccad30db4c6383a48490848
anseee <parksungwon1113@gmail.com>
2021-12-29 22:33:08 +0900

    parameter generics

--- Advanced Swift Generics and Protocols/GenericsAndProtocols.playground/Contents.swift
+++ Advanced Swift Generics and Protocols/GenericsAndProtocols.playground/Contents.swift
@@ -1,3 +1,39 @@
-import UIKit
+// Copyright (c) 2019 Razeware LLC
+// See Copyright Notice page for details about the license.
 
-var greeting = "Hello, playground"
+struct Stack<Element> {
+  
+  private var storage: [Element] = []
+  
+  mutating func push(_ element: Element) {
+    storage.append(element)
+  }
+  
+  mutating func pop() -> Element? {
+    return storage.popLast()
+  }
+  
+  var top: Element? {
+    return storage.last
+  }
+  
+  var isEmpty: Bool {
+    return top == nil
+  }
+  
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self.storage = elements
+    }
+}
+
+var stack: Stack = [1,2,3]
+stack.push(1)
+stack.pop()
+
+
+
+
+
